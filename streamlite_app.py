37baee1b4bdeb860b6344c63fd53996d2ed1313c
davisagra <dagra.rus@gmail.com>
2022-09-28 09:50:09 +0200

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -1,8 +1,8 @@
 import streamlit
 
-streamlit.title('My parents New Healthy Dinner')
-streamlit.header('Breakfast menu')
-streamlit.text('Kale, Rocket')
-streamlit.text('potatoes, eggs')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
 
